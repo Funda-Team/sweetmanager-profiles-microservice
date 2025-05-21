8e34c4a042b5ef1efd56915687dcdacebcecd4f1
Giacomo202210029 <u202210029@upc.edu.pe>
2025-05-20 23:42:47 -0500

    feat: Added shared

---
+++ ProfilesService/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using IamService.Shared.Domain.Repositories;
+
+namespace IamService.Shared.Infrastructure.Persistence.EFC.Repositories
+{
+    public class UnitOfWork(ProfilesContext context) : IUnitOfWork
+    {
+        public async Task CommitAsync() => await context.SaveChangesAsync();
+    }
+}
