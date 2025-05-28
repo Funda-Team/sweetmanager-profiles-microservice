5570617190cb235ff9f9875bf1be1d144f9f9d79
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:51:47 -0500

    feat: added command and query service for Provider

---
+++ ProfilesService/Application/Internal/CommandService/ProviderCommandService.cs
@@ -0,0 +1,30 @@
+﻿using ProfilesService.Domain.Model.Commands.Provider;
+using ProfilesService.Domain.Repositories;
+using ProfilesService.Domain.Services.Provider;
+using ProfilesService.Shared.Domain.Repositories;
+
+namespace ProfilesService.Application.Internal.CommandService;
+
+public class ProviderCommandService(IProviderRepository providerRepository, IUnitOfWork unitOfWork): IProviderCommandService
+{
+    public async Task<bool> Handle(CreateProviderCommand command)
+    {
+        try
+        {
+            await providerRepository.AddAsync(new(command));
+            
+            await unitOfWork.CommitAsync();
+            
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    public async Task<bool> 
+        Handle(UpdateProviderCommand command)=> 
+        await providerRepository.UpdateProviderStateAsync
+        (command.Id,command.Address,command.Email,command.Phone,command.State);
+    
+}
