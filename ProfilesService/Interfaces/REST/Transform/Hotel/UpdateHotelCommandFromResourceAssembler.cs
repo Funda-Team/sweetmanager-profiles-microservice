b57f7d990247ca5ee16f76e36b3aa8982d271afa
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:54:05 -0500

    feat: added Create from resource and update command for Hotel

---
+++ ProfilesService/Interfaces/REST/Transform/Hotel/UpdateHotelCommandFromResourceAssembler.cs
@@ -0,0 +1,10 @@
+﻿using ProfilesService.Domain.Model.Commands.Hotel;
+using ProfilesService.Interfaces.REST.Resources.Hotel;
+
+namespace ProfilesService.Interfaces.REST.Transform.Hotel;
+
+public class UpdateHotelCommandFromResourceAssembler
+{
+    public static UpdateHotelCommand ToCommandFromResource(UpdateHotelResource resource)=>
+    new(resource.Id,resource.Name,resource.Phone,resource.Email);
+}
