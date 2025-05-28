7755a96ac638d8f88d2904060aad83c87a9c5a9e
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:54:15 -0500

    feat: added Create from resource and update command for Provider

---
+++ ProfilesService/Interfaces/REST/Transform/Provider/ProviderResourceFromEntityAssembler.cs
@@ -0,0 +1,9 @@
+﻿using ProfilesService.Interfaces.REST.Resources.Provider;
+
+namespace ProfilesService.Interfaces.REST.Transform.Provider;
+
+public class ProviderResourceFromEntityAssembler
+{
+    public static ProviderResource ToResourceFromEntity(Domain.Model.Aggregates.Provider entity) =>
+        new(entity.Id, entity.Name, entity.Address, entity.Email, entity.Phone, entity.State);
+}
---
+++ ProfilesService/Interfaces/REST/Transform/Provider/UpdateProviderCommandFromResourceAssembler.cs
@@ -0,0 +1,10 @@
+﻿using ProfilesService.Domain.Model.Commands.Provider;
+using ProfilesService.Interfaces.REST.Resources.Provider;
+
+namespace ProfilesService.Interfaces.REST.Transform.Provider;
+
+public class UpdateProviderCommandFromResourceAssembler
+{
+    public static UpdateProviderCommand ToCommandFromResource(UpdateProviderResource resource) => 
+    new(resource.Id,resource.Address,resource.Email,resource.Phone,resource.State);
+}
