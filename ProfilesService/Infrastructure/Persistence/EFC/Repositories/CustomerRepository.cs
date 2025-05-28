a090721267b8c17f1674e06644dff0e19cd263f1
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:50:47 -0500

    feat: added repository for Customer

---
+++ ProfilesService/Infrastructure/Persistence/EFC/Repositories/CustomerRepository.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using ProfilesService.Domain.Model.Aggregates;
+using ProfilesService.Domain.Model.ValueObject;
+using ProfilesService.Domain.Repositories;
+using ProfilesService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using ProfilesService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace ProfilesService.Infrastructure.Persistence.EFC.Repositories;
+
+public class CustomerRepository : BaseRepository<Customer>, ICustomerRepository
+{
+    private readonly ProfilesContext _context;
+    
+    public CustomerRepository(ProfilesContext context) : base(context)
+    {
+        _context = context;
+    }
+    
+    public async Task<bool> UpdateCustomerStateAsync(int id, string email, int phone, StateType state) =>
+        await _context.Set<Customer>()
+            .Where(c =>c.Id == id)
+            .ExecuteUpdateAsync( c =>c
+                .SetProperty(u => u.Email, email)
+                .SetProperty(u => u.Phone, phone)
+                .SetProperty(u => u.State, state)
+            ) > 0;
+
+    public Task<IEnumerable<Customer>> FindCustomerByHotelIdAsync(int hotelId)
+    {
+        throw new NotImplementedException();
+    }
+}
