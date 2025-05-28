3ba1b0943311b512e6c2bbfec20480f9f6b676a1
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:53:51 -0500

    feat: added Create from resource and update command for Customer

---
+++ ProfilesService/Interfaces/REST/Transform/Customer/CreateCustomerCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using ProfilesService.Domain.Model.Commands.Customer;
+using ProfilesService.Interfaces.REST.Resources.Customer;
+
+namespace ProfilesService.Interfaces.REST.Transform.Customer;
+
+public class CreateCustomerCommandFromResourceAssembler
+{
+    public static CreateCustomerCommand ToCommandFromResource(CreateCustomerResource resource)=>
+    new(resource.Username, resource.Name, resource.Username, resource.Email, resource.Phone);
+    
+}
+
