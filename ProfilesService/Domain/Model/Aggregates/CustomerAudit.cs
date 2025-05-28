883f71328c5116c9e9e038876cc8cfd1cbe5627c
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:48:40 -0500

    feat: added audit for aggregates

---
+++ ProfilesService/Domain/Model/Aggregates/CustomerAudit.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace ProfilesService.Domain.Model.Aggregates;
+
+public partial class Customer : IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
