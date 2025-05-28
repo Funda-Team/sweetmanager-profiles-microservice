bf8df25d3c15f1c9ed12511117dfa05096523ea4
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:53:20 -0500

    feat: added Create, Update and Resource for Provider

---
+++ ProfilesService/Interfaces/REST/Resources/Provider/CreateProviderResource.cs
@@ -0,0 +1,6 @@
+﻿using ProfilesService.Domain.Model.ValueObject;
+
+namespace ProfilesService.Interfaces.REST.Resources.Provider;
+
+public record CreateProviderResource(string Name, string Address, string Email, int Phone);
+
---
+++ ProfilesService/Interfaces/REST/Resources/Provider/ProviderResource.cs
@@ -0,0 +1,5 @@
+﻿using ProfilesService.Domain.Model.ValueObject;
+
+namespace ProfilesService.Interfaces.REST.Resources.Provider;
+
+public record ProviderResource(int Id, string Name, string Address, string Email, int Phone, StateType State);
