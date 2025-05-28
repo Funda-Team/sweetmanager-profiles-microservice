2f3992b9d77917f975ffb5c7e7370341c2b8e301
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:51:40 -0500

    feat: added command and query service for Hotel

---
+++ ProfilesService/Application/Internal/CommandService/HotelCommandService.cs
@@ -0,0 +1,26 @@
+﻿using ProfilesService.Domain.Model.Commands.Hotel;
+using ProfilesService.Domain.Repositories;
+using ProfilesService.Domain.Services.Hotel;
+using ProfilesService.Shared.Domain.Repositories;
+
+namespace ProfilesService.Application.Internal.CommandService;
+
+public class HotelCommandService(IHotelRepository hotelRepository,IUnitOfWork unitOfWork):IHotelCommandService
+{
+    public async Task<bool> Handle(CreateHotelCommand command)
+    {
+        try
+        {
+            await hotelRepository.AddAsync(new(command));
+            await unitOfWork.CommitAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    
+    public async Task<bool> Handle(UpdateHotelCommand command)=> 
+    await hotelRepository.UpdateHotelStateAsync(command.Id,command.Name,command.Phone,command.Email);
+}
---
+++ ProfilesService/Application/Internal/QueryService/HotelQueryService.cs
@@ -0,0 +1,25 @@
+﻿using ProfilesService.Domain.Model.Entities;
+using ProfilesService.Domain.Model.Queries.Hotel;
+using ProfilesService.Domain.Repositories;
+using ProfilesService.Domain.Services.Hotel;
+
+namespace ProfilesService.Application.Internal.QueryService;
+
+public class HotelQueryService: IHotelQueryService
+{
+    private readonly IHotelRepository _hotelRepository;
+
+    public HotelQueryService(IHotelRepository hotelRepository)
+    {
+        _hotelRepository = hotelRepository;
+    }
+    public async Task<IEnumerable<Hotel>> handle(GetAllHotelsQuery query)
+    {
+        return await _hotelRepository.GetAllAsync();
+    }
+
+    public async Task<Hotel?> handle(GetHotelByOwnersIdQuery query)
+    {
+        return await _hotelRepository.GetByOwnersIdAsync(query.OwnerId);
+    }
+}
