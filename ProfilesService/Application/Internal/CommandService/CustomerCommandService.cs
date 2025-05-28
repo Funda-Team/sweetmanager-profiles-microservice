2c0c039cdd76259e34506e1642e923de0926cec0
U202210138 <u202210138@upc.edu.pe>
2025-05-27 22:51:30 -0500

    feat: added command and query service for Customer

---
+++ ProfilesService/Application/Internal/CommandService/CustomerCommandService.cs
@@ -0,0 +1,28 @@
+﻿using ProfilesService.Domain.Model.Commands.Customer;
+using ProfilesService.Domain.Repositories;
+using ProfilesService.Domain.Services.Customer;
+using ProfilesService.Shared.Domain.Repositories;
+
+namespace ProfilesService.Application.Internal.CommandService;
+
+public class CustomerCommandService(ICustomerRepository customerRepository, IUnitOfWork unitOfWork):ICustomerCommandService
+{
+    public async Task<bool> Handle(CreateCustomerCommand command)
+    {
+        try
+        {
+            await customerRepository.AddAsync(new(command));
+            await unitOfWork.CommitAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    
+    public async Task<bool> Handle
+        (UpdateCustomerCommand command) => 
+        await customerRepository.UpdateCustomerStateAsync
+        (command.Id,command.Email, command.Phone,command.State);
+}
---
+++ ProfilesService/Application/Internal/QueryService/CustomerQueryService.cs
@@ -0,0 +1,14 @@
+﻿using ProfilesService.Domain.Model.Aggregates;
+using ProfilesService.Domain.Model.Queries;
+using ProfilesService.Domain.Repositories;
+using ProfilesService.Domain.Services.Customer;
+
+namespace ProfilesService.Application.Internal.QueryService
+{
+    public class CustomerQueryService (ICustomerRepository customerRepository) : ICustomerQueryService
+    {
+        public async Task<IEnumerable<Customer>> Handle(GetAllCustomersQuery query) => await customerRepository.FindCustomerByHotelIdAsync(query.HotelId);
+    
+    
+    }
+}
