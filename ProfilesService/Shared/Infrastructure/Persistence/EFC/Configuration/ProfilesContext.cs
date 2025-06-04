4b1be328bc80c4ab00611b9630d322eef11a486f
U202210138 <u202210138@upc.edu.pe>
2025-06-04 01:19:56 -0500

    feat: updated the profiles database

--- ProfilesService/Shared/Infrastructure/Persistence/EFC/Configuration/ProfilesContext.cs
+++ ProfilesService/Shared/Infrastructure/Persistence/EFC/Configuration/ProfilesContext.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using ProfilesService.Domain.Model.Aggregates;
 using ProfilesService.Domain.Model.Entities;
+using ProfilesService.Domain.Model.ValueObject;
 
 namespace ProfilesService.Shared.Infrastructure.Persistence.EFC.Configuration;
 
@@ -18,38 +18,46 @@ public partial class ProfilesContext : DbContext
     }
 
     public virtual DbSet<Customer> Customers { get; set; }
-
     public virtual DbSet<Hotel> Hotels { get; set; }
-
     public virtual DbSet<Provider> Providers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=localhost;database=profiles;user=root;password=password;");
+    {
+        // Connection string is already provided via DI in Program.cs
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Customer>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("customers");
-
             entity.Property(e => e.Id).HasColumnName("id");
+
+            // Map the audit fields using the correct property names
+            entity.Property(e => e.CreatedDate).HasColumnName("CreatedAt");
+            entity.Property(e => e.UpdatedDate).HasColumnName("UpdatedAt");
+
             entity.Property(e => e.Email)
                 .HasMaxLength(100)
                 .HasColumnName("email");
+
             entity.Property(e => e.HotelsId).HasColumnName("hotels_id");
+
             entity.Property(e => e.Name)
                 .HasMaxLength(100)
                 .HasColumnName("name");
+
             entity.Property(e => e.Phone).HasColumnName("phone");
+
             entity.Property(e => e.State)
-                .HasMaxLength(20)
-                .HasColumnName("state");
+                .HasColumnName("state")
+                .HasConversion<string>();
+
             entity.Property(e => e.Surname)
                 .HasMaxLength(100)
                 .HasColumnName("surname");
+
             entity.Property(e => e.Username)
                 .HasMaxLength(100)
                 .HasColumnName("username");
@@ -58,22 +66,25 @@ public partial class ProfilesContext : DbContext
         modelBuilder.Entity<Hotel>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("hotels");
-
             entity.Property(e => e.Id).HasColumnName("id");
+
             entity.Property(e => e.Address)
                 .HasMaxLength(500)
                 .HasColumnName("address");
+
             entity.Property(e => e.Description)
                 .HasMaxLength(5000)
                 .HasColumnName("description");
+
             entity.Property(e => e.Email)
                 .HasMaxLength(100)
                 .HasColumnName("email");
+
             entity.Property(e => e.Name)
                 .HasMaxLength(100)
                 .HasColumnName("name");
+
             entity.Property(e => e.OwnersId).HasColumnName("owners_id");
             entity.Property(e => e.Phone).HasColumnName("phone");
         });
@@ -81,24 +92,32 @@ public partial class ProfilesContext : DbContext
         modelBuilder.Entity<Provider>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("providers");
-
             entity.Property(e => e.Id).HasColumnName("id");
+
+            // Map the audit fields using the correct property names
+            entity.Property(e => e.CreatedDate).HasColumnName("CreatedAt");
+            entity.Property(e => e.UpdatedDate).HasColumnName("UpdatedAt");
+
             entity.Property(e => e.Address)
                 .HasMaxLength(250)
                 .HasColumnName("address");
+
             entity.Property(e => e.Email)
                 .HasMaxLength(100)
                 .HasColumnName("email");
+
             entity.Property(e => e.HotelsId).HasColumnName("hotels_id");
+
             entity.Property(e => e.Name)
                 .HasMaxLength(100)
                 .HasColumnName("name");
+
             entity.Property(e => e.Phone).HasColumnName("phone");
+
             entity.Property(e => e.State)
-                .HasMaxLength(20)
-                .HasColumnName("state");
+                .HasColumnName("state")
+                .HasConversion<string>();
         });
 
         OnModelCreatingPartial(modelBuilder);
